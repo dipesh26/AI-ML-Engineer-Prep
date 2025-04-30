7015bc6d32c28de6b3e62269a755564accd10c2c
Dipesh Mahajan | AI/ML Engineer <dipeshmahajan26@gmail.com>
2025-04-30 11:30:28 +0530

    Delete escapesequencecharacters.py

--- escapesequencecharacters.py
+++
@@ -1,3 +0,0 @@
-print("Hey, My name is Dipesh Mahajan & i am a Video Editor.")
-print("Hey, My name is Dipesh Mahajan \ni am a Video Editor.")
-print("Hey, My name is \"Dipesh Mahajan\" & i am a \"Video Editor.\"")

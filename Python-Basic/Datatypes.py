69d432573fd20cb80bf16f422757d1ddf34ee27e
Dipesh Mahajan | AI/ML Engineer <dipeshmahajan26@gmail.com>
2025-04-30 11:37:01 +0530

    Add files via upload

---
+++ Python-Basic/Datatypes.py
@@ -0,0 +1,54 @@
+# Data type is refers to the type of data that are stored in the variables.
+# Type of Data Types ->
+
+# variables      #data types
+a = "Hello World"                      # str Type
+b = 20                                 # int Type
+c =  20.5                              # float Type
+d = 1j                                 # complex Type
+e = ["Orange", "Banana", "Cherry"]     # list Type
+f = ("Orange", "Banana", "Cherry")     # tuple Type 
+g = range(6)                           # range Type
+h = {"Name":" Dipesh", "Age":" 22"}    # dict Type
+i = True                               # bool Type
+j = None                               # None Type
+
+print(a)
+print(type(a))
+print("\n")
+
+print(b)
+print(type(b))
+print("\n")
+
+print(c)
+print(type(c))
+print("\n")
+
+print(d)
+print(type(d))
+print("\n")
+
+print(e)
+print(type(e))
+print("\n")
+
+print(f)
+print(type(f))
+print("\n")
+
+print(g)
+print(type(g))
+print("\n")
+
+print(h)
+print(type(h))
+print("\n")
+
+print(i)
+print(type(i))
+print("\n")
+
+print(j)
+print(type(j))
+print("\n")

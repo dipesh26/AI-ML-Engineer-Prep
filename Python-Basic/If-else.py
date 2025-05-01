0ff1716ae9c113af5f1d8341059466cf693d366d
Dipesh Mahajan | AI/ML Engineer <dipeshmahajan26@gmail.com>
2025-05-01 11:37:37 +0530

    Add files via upload

---
+++ Python-Basic/If-else.py
@@ -0,0 +1,18 @@
+# if-else program 1
+age = int(input("Enter your Age: "))
+
+if(age >= 18):
+    print("You can Drive the car")
+else:
+    print("You can not Drive the car")
+print("\n")
+
+# if-else program 2
+appleprice = 200
+budget = int(input("Enter your Budget: "))
+
+if(appleprice <= budget):
+    print("You can buy the apples")
+else:
+    print("You can not buy the apples")
+print("Thank You :)")

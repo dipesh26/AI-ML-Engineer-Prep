2ec6fa96d0b6ba6b53069db625c54c021e0e351e
Dipesh Mahajan | AI/ML Engineer <dipeshmahajan26@gmail.com>
2025-07-04 15:47:18 +0530

    Add files via upload

---
+++ Practice Problem/Oops/10_Que.py
@@ -0,0 +1,34 @@
+# Demonstrate polymorphism by defining a method fuel_type in both Car and ElectricCar classes, but with different behaviors.
+
+class Car:
+    def __init__(self, brand, model):
+        self.__brand = brand
+        self.model = model
+
+    def car_info(self):
+        print(f"Brand : {self.__brand}")
+        print(f"Model : {self.model}")
+
+    def get_brand(self):
+        return self.__brand
+    
+    def fuel_type(self):
+        return "Petrol and Deisel"
+    
+class ElectricCar(Car):
+    def __init__(self, brand , model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+
+    def fuel_type(self):
+        return "Electric Charge"
+
+car1 = ElectricCar("Mahindra","XUV400 EV","39.4 kWh")
+car2 = Car("Toyota","Fortuner")
+
+car1.car_info()
+print(f"Battery Size : {car1.battery_size}")
+print(f"Type : {car1.fuel_type()}\n")
+
+car2.car_info()
+print(f"Type : {car2.fuel_type()}\n")

4c7941ec3e84e81acde697ef51a490644dd7d229
Dipesh <dipeshmahajan26@gmail.com>
2025-07-12 16:07:48 +0530

    Updated web_scarping_ecom.py

--- Python Projects/Web Data Scraper/web_scarping_ecom.py
+++ Python Projects/Web Data Scraper/web_scarping_ecom.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-data = {"Title" : [], "Price" : [], "Stock" : []}
+data = {"Title" : [], "Price" : []}
 
 url = "https://books.toscrape.com/"
 

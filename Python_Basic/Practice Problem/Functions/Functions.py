d775cc306a3588520e509c45ebd67006cfb3090c
Dipesh <dipeshmahajan26@gmail.com>
2025-07-14 15:10:56 +0530

    Updated 'Functions.py'

--- Python_Basic/Practice Problem/Functions/Functions.py
+++ Python_Basic/Practice Problem/Functions/Functions.py
@@ -1,55 +1,55 @@
 # 1: Calculate the average of 3 numbers.
-# def avg_calc(a,b,c):
-#     sum = a+b+c
-#     avg = sum/3
-#     print(f"Average is {avg}")
-#     return avg
+def avg_calc(a,b,c):
+    sum = a+b+c
+    avg = sum/3
+    print(f"Average is {avg}")
+    return avg
 
-# a = int(input("Enter the 1st number: "))
-# b = int(input("Enter the 2nd number: "))
-# c = int(input("Enter the 3rd number: "))
-# avg_calc(a,b,c)
+a = int(input("Enter the 1st number: "))
+b = int(input("Enter the 2nd number: "))
+c = int(input("Enter the 3rd number: "))
+avg_calc(a,b,c)
 
 # 2: Write a function to print the length of a list:
-# cities = ['Burhanpur','Indore','Pune','Ujjain','Mumbai','Lonavala','Noida','Delhi']
-# states = ['M.P','U.P','Maharashtra','Chattisgardh','Karnatak','Tamil Nadu']
-# def len_find(list):
-#     print(len(list))
+cities = ['Burhanpur','Indore','Pune','Ujjain','Mumbai','Lonavala','Noida','Delhi']
+states = ['M.P','U.P','Maharashtra','Chattisgardh','Karnatak','Tamil Nadu']
+def len_find(list):
+    print(len(list))
 
-# len_find(cities)
-# len_find(states)
+len_find(cities)
+len_find(states)
 
 # 3: Write a function to print the elements of a list in a single line. (list is the parameter)
-# cities = ['Burhanpur','Indore','Pune','Ujjain','Mumbai','Lonavala','Noida','Delhi']
-# states = ['M.P','U.P','Maharashtra','Chattisgardh','Karnatak','Tamil Nadu']
-# def print_list_element(list):
-#     for i in list:
-#         print(i,end=" ")
+cities = ['Burhanpur','Indore','Pune','Ujjain','Mumbai','Lonavala','Noida','Delhi']
+states = ['M.P','U.P','Maharashtra','Chattisgardh','Karnatak','Tamil Nadu']
+def print_list_element(list):
+    for i in list:
+        print(i,end=" ")
 
-# print_list_element(cities)
+print_list_element(cities)
 
 # 4: Write a function to find the factorial of n. (n is the parameter)
-# def factorial(n):
-#     fact = 1
-#     for i in range(1, n+1):
-#         fact *= i
-#     print(f"Factorial of {n} is: {fact}")
+def factorial(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact *= i
+    print(f"Factorial of {n} is: {fact}")
 
-# factorial(int(input("Enter the Number: ")))
+factorial(int(input("Enter the Number: ")))
 
 # 5: Write a function to convert USD to INR.
-# def USD_convert(USD):
-#     INR = 86.58 * USD
-#     print(f"{USD} USD = {INR} INR")
+def USD_convert(USD):
+    INR = 86.58 * USD
+    print(f"{USD} USD = {INR} INR")
 
-# USD_convert(int(input("Enter the Amount in USD: ")))
+USD_convert(int(input("Enter the Amount in USD: ")))
 
 # 6: Write a function to check the number is even or odd.
-# def Odd_Even_checker(num):
-#     if num % 2 == 0:
-#         print(f"The Number {num} is \"Even\"")
-#     elif num % 2 != 0:
-#         print(f"The Number {num} is \"Odd\"")
-#     return num
+def Odd_Even_checker(num):
+    if num % 2 == 0:
+        print(f"The Number {num} is \"Even\"")
+    elif num % 2 != 0:
+        print(f"The Number {num} is \"Odd\"")
+    return num
 
-# Odd_Even_checker(int(input("Enter the Number: ")))
+Odd_Even_checker(int(input("Enter the Number: ")))

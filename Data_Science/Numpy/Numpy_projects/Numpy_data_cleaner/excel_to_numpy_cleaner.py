17f0645998076277cd5a20e61bca2e262a75e028
Dipesh <dipeshmahajan26@gmail.com>
2025-07-26 15:38:33 +0530

    Updated 'Numpy_data_cleaner'

--- Data_Science/Numpy/Numpy_projects/Numpy_data_cleaner/excel_to_numpy_cleaner.py
+++ Data_Science/Numpy/Numpy_projects/Numpy_data_cleaner/excel_to_numpy_cleaner.py
@@ -15,15 +15,14 @@ def clean_missing_values(data):
     return data
 
 def normalizing_cleaned_data(cleaned_data):
-    global data_min, data_max
     data_min = np.min(cleaned_data)
     data_max = np.max(cleaned_data)
     
     normalized_data = (cleaned_data - data_min) / (data_max - data_min)
     print("✅ Data Normalized...")
-    return normalized_data
+    return normalized_data, data_max, data_min
 
-def denormalizing_data(normalized_data):
+def denormalizing_data(normalized_data, data_max, data_min):
     original_data = normalized_data * (data_max - data_min) + data_min
     print("\n✅ Data Denormalized successfully.")
     return original_data
@@ -37,24 +36,37 @@ def main():
             print("\n❗File name can't be empty.")
             continue
         try:
+            with open(user_input, 'r') as f:
+                headers = f.readline().strip().split(',')
+            
             with warnings.catch_warnings():
                 warnings.simplefilter('ignore', category=UserWarning)
                 load_file = np.genfromtxt(user_input, delimiter=',', skip_header=1)
+            
             if load_file.size == 0:
                 print("\n❗File is empty or only contains headers.")
                 continue
+            
             print("\n✅ File loaded...")
             
             cleaned_data = clean_missing_values(load_file)
-            normalized_data = normalizing_cleaned_data(cleaned_data)
-            print("\nNormalized & Cleaned Data:")
-            print(f"--------------------------\n\n{normalized_data}")
+            normalized_data, data_max, data_min = normalizing_cleaned_data(cleaned_data)
             
-            option = str(input("\nSee Original Data? [y]: ")).strip().lower()
+            print("\nNormalized & Cleaned Data:")
+            print("--------------------------")
+            print("\t".join(headers))
+            for row in normalized_data:
+                print("\t".join(f"{val:.2f}" for val in row))
+            
+            option = str(input("\nSee Original Cleaned Data? [y]: ")).strip().lower()
             if option == 'y':
-                original_data = denormalizing_data(normalized_data)
-                print("\nOriginal Data:")
-                print(f"--------------\n\n{original_data}\n")
+                original_data = denormalizing_data(normalized_data, data_max, data_min)
+                print("\nOriginal Cleaned Data:")
+                print("----------------------")
+                print("\t".join(headers))
+                for row in original_data:
+                    print("\t".join(f"{val:.2f}" for val in row))
+                print("\n")
                 break
             else:
                 print("\n")
